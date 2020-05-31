e1b09fb920eade43f3df8ddd81fd4848cf07deca
Lokres <aleksvalmix@yandex.ru>
2020-05-31 23:35:13 +0300

    minifix

--- JsPhpExec.py
+++ JsPhpExec.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Author: user
 # @Date:   2020-05-31 22:10:07
-# @Last Modified by:   user
-# @Last Modified time: 2020-05-31 22:42:01
+# @Last Modified by:   Lokres
+# @Last Modified time: 2020-05-31 23:34:25
 
 
 import sublime
 import sublime_plugin
 import subprocess
-
+import re
 class JsPhpExec(sublime_plugin.TextCommand):
     def run(self, edit):
 
@@ -16,5 +16,10 @@ class JsPhpExec(sublime_plugin.TextCommand):
         sel = view.sel()
         text = sel[0]
         cmd = view.substr(text)
-        res = subprocess.check_output(['node', '-e', cmd, '--print'], shell=True).decode('UTF-8')
+
+        import re
+        cmd = re.sub(r'\n\s*{', r'{', cmd).lower()
+        cmd = re.sub(r'\n\s*([^\s{])', r';\1', cmd)
+
+        res = subprocess.check_output(['node', '--eval', cmd, '--print'], shell=True).decode('UTF-8')
         self.view.show_popup(res)
